74ffc9d3f595d1e5c01a5ec433a4381e4828ac42
Fedor Batonogov <batonogov@icloud.com>
2020-09-05 10:56:15 +0300

    Update start_server.sh

--- cron.reboot/start_server.sh
+++ cron.reboot/start_server.sh
@@ -1,10 +1,9 @@
-DIR="/minecraft_server"
-if [[ -d "$DIR" && "$(ls -A $DIR)" ]]
+if [ -f minecraft_server/server.jar ]
 then
     cd /minecraft_server/ 
-    java -Xmx1024M -Xms1024M -jar server.jar nogui
+    java -Xmx2048M -Xms2048M -jar server.jar nogui
 else
     cd /minecraft_server/ 
     wget https://launcher.mojang.com/v1/objects/c5f6fb23c3876461d46ec380421e42b289789530/server.jar 
-    java -Xmx1024M -Xms1024M -jar server.jar nogui
+    java -Xmx2048M -Xms2048M -jar server.jar nogui
 fi
